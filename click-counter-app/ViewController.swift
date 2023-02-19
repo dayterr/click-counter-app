300180aca22f3eb7b35ed065120395e9c5b380ea
dayterr <mvdayter@gmail.com>
2023-02-20 00:55:18 +0300

    added label and button

--- click-counter-app/ViewController.swift
+++ click-counter-app/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var countButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countLabel.text = "Значение счётчика: 0"
+        countButton.setTitle("Кнопочка", for: .normal)
     }
 
-
+    var counter: Int = 0
+    
+    @IBAction func increase(_ sender: Any) {
+        counter += 1
+        countLabel.text = "Значение счётчика: \(counter)"
+    }
 }
 
