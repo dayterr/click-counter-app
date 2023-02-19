94dbb12ba7e52699adbca081fffe59849f5102c0
dayterr <mvdayter@gmail.com>
2023-02-20 00:16:49 +0300

    Initial Commit

---
+++ click-counter-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  click-counter-app
+//
+//  Created by Ruth Dayter on 20.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
